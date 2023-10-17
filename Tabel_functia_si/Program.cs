921508b4ef8869291148ba593f6de09013ff3757
denisdamse <146414688+denisdamse@users.noreply.github.com>
2023-10-17 19:42:43 +0300

    Add files via upload

---
+++ Tabel_functia_si/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Tabel_functia_si;
+class Program
+{
+    static void Main(string[] args)
+    { 
+        Console.WriteLine("True & True = " + (true & true));
+        Console.WriteLine("True & False = " + (true & false));
+        Console.WriteLine("False & True = " + (false & true));
+        Console.WriteLine("False & False = " + (false & false));
+    }
+}
+
